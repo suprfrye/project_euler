7176c08014fe66c07342e79b91d123729b925086
suprfrye <tara.c.frye@gmail.com>
2015-07-19 17:17:01 -0400

    Define largest_palindrome_product method and call

--- largest_palindrome_product.rb
+++ largest_palindrome_product.rb
@@ -1,3 +1,15 @@
 # A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 
 # Find the largest palindrome made from the product of two 3-digit numbers.
+
+def largest_palindrome_product
+  palindromes = []
+  for i in (900..999) do
+    for j in (900..999) do
+      palindromes << i*j if ((i*j).to_s == (i*j).to_s.reverse)
+    end
+  end
+  palindromes.max
+end
+
+p largest_palindrome_product
