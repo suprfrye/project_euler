c52fd20b352e90b84630976d73bfc1250b5aa2e0
suprfrye <tara.c.frye@gmail.com>
2015-07-19 16:05:39 -0400

    Define largest_prime_factor and call it with 600851475143.

--- largest_prime_factor.rb
+++ largest_prime_factor.rb
@@ -1,3 +1,21 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 
 # What is the largest prime factor of the number 600851475143 ?
+
+def largest_prime_factor(num)
+  prime_factors = []
+  divisor = 0
+  i = 2
+  until divisor == 1 do
+    if num % i == 0
+      num /= i
+      divisor = num
+      prime_factors  << i
+    else
+      i += 1
+    end
+  end
+  prime_factors.max
+end
+
+p largest_prime_factor(600851475143)
