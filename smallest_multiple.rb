0e6a4ad1ebe7375ae3703fdbb12f87f100d3f487
suprfrye <tara.c.frye@gmail.com>
2015-07-19 20:52:28 -0400

    Define smallest_multiple and call

--- smallest_multiple.rb
+++ smallest_multiple.rb
@@ -1,3 +1,14 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+def smallest_multiple
+  multiple = 2520
+  while true do
+    factors = (1..20).select {|num| multiple % num == 0}
+    factors.length == 20 ? break : multiple += 15
+  end
+  multiple
+end
+
+p smallest_multiple # It takes a while, but the number is 232792560.
